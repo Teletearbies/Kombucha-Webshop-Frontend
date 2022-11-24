a61946f8b904f270a84c03de9be9ec50e70c2e85
Dániel Szabó <kingalistairlol@gmail.com>
2022-11-24 14:12:51 +0100

    Product page fetch works, organized packages and names.

--- JS/Service/API_calls.js
+++ JS/Service/API_calls.js
@@ -4,3 +4,39 @@ export async function getAll(resource) {
     return  fetch(url + '/'+ resource)
         .then(response => response.json())
 }
+
+export async function getById(id, resource) {
+    return  fetch (url + '/'+ resource+'/' + id )
+        .then(response => response.json())
+}
+
+export async function deleteById(id, resource) {
+    return  fetch(url + '/'+ resource+'/' + id,{
+        method:"DELETE"
+    })
+        .then(response => {
+            if(response.status===200){
+                return  "successfully deleted item"
+            }
+            return response.status
+        })
+        .catch(err => console.log(err))
+}
+
+export async function update(id,body,resource){
+    return  fetch(url + '/'+ resource+'/' + id,{
+        method:"PUT",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    })
+        .then(response => {
+            if(response.status===200){
+                return  "successfully updated item"
+            }
+            return response.status
+        })
+        .catch(err => console.log(err))
+
+}
