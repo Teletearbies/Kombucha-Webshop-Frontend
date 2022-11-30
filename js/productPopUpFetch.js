b2d31235d1fd8f52a56c3ef2f36c7881c3177b25
Dániel Szabó <kingalistairlol@gmail.com>
2022-11-30 12:35:56 +0100

    Product popup in progress

--- js/productPopUpFetch.js
+++ js/productPopUpFetch.js
@@ -6,14 +6,14 @@ const productPrice = document.getElementById("productPrice");
 //Get id from URL sent by products.html
 const param = new URLSearchParams(window.location.search);
 const id = param.get("id");
-const apiUrl = "http://localhost:8080/products/" + 1
+const apiUrl = "http://localhost:8080/products/" + id
 
 
 async function getIt() {
     const response = await fetch(apiUrl);
     const product = await response.json();
     let div = document.getElementById("image_column")
-    div.style.backgroundImage = div.style.backgroundImage = "linear-gradient(45deg, rgba(44, 174, 186, 0.1), rgba(44, 174, 186, 0.1)), url(../img/products/icon/" + 1 + ".png" + ")"
+    div.style.backgroundImage = div.style.backgroundImage = "linear-gradient(45deg, rgba(44, 174, 186, 0.1), rgba(44, 174, 186, 0.1)), url(../img/products/icon/" + id + ".png" + ")"
     productName.textContent = product.name;
     productDescription.textContent = product.description;
     productPrice.textContent = product.price + ",-" ;
