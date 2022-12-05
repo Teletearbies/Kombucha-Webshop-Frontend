920361b27fae2f5bc242b65e3bbd1102b98c9127
Dániel Szabó <kingalistairlol@gmail.com>
2022-12-05 18:22:23 +0100

    Customer feedback on Product title's done on Front page, Product Page and Admin Product Page

--- js/admin/adminProductFetch.js
+++ js/admin/adminProductFetch.js
@@ -8,22 +8,41 @@ getAll("products").then(r => {
     r.forEach(item => {
         let a = document.createElement("a")
         let div = document.createElement("div")
-        div.className = "products"
-        a.id = item.id
-        div.innerText = item.name
+        let productName = document.createElement("productName")
+
+        div.className = "products";
+
+        a.id = item.id;
         a.style.textDecorationLine = "none";
+
+        productName.style.backgroundColor = "rgba(255,255,255,0.8)";
+        productName.style.borderRadius ="25px 25px 0 0";
+        productName.innerText = item.name;
+        productName.style.display = "none";
+
+        div.style.transition = "1s";
         div.style.color = "#fc9721";
         div.style.textAlign = "CENTER";
         div.style.fontSize = "26px";
         div.style.backgroundImage = "linear-gradient(45deg, rgba(44, 174, 186, 0.1), rgba(44, 174, 186, 0.1)), url(../../img/products/thumbnails/" + item.id + ".png"+")"
-        console.log(div.style.backgroundImage)
 
         a.addEventListener('click', () =>{
-            productId.value = item.id
+            productId.value = item.id;
+
             $('#cont').load("adminProductPopUpPage.html")
         })
 
+        a.addEventListener('mouseover', function handleMouseOver() {
+            productName.style.display = 'block';
+
+        });
+
+        a.addEventListener('mouseout', function handleMouseOut() {
+            productName.style.display = 'none';
+        });
+
         a.appendChild(div)
+        div.appendChild(productName)
         products.appendChild(a)
     })
 })
