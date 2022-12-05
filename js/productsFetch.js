e355805a0a189dbcdb172b23ff97e5c38186d848
Dániel Szabó <kingalistairlol@gmail.com>
2022-12-05 14:39:17 +0100

    Product page hover transitions

--- js/productsFetch.js
+++ js/productsFetch.js
@@ -8,23 +8,40 @@ getAll("products").then(r => {
     r.forEach(item => {
         let a = document.createElement("a")
         let div = document.createElement("div")
-        div.className = "products"
-        a.id = item.id
-        div.innerText = item.name
+        let productName = document.createElement("productName")
+
+        div.className = "products";
+        a.id = item.id;
+        productName.innerText = item.name;
+        productName.style.display = "none";
         a.style.textDecorationLine = "none";
+        div.style.transition = "2s";
         div.style.color = "#fc9721";
         div.style.textAlign = "CENTER";
         div.style.fontSize = "26px";
         div.style.backgroundImage = "linear-gradient(45deg, rgba(44, 174, 186, 0.1), rgba(44, 174, 186, 0.1)), url(../../img/products/thumbnails/" + item.id + ".png"+")"
-        console.log(div.style.backgroundImage)
 
         a.addEventListener('click', () =>{
-            productId.value = item.id
-            console.log("clicked the event")
+            productId.value = item.id;
+
             $('#cont').load("popUpPage.html")
         })
 
+        a.addEventListener('mouseover', function handleMouseOver() {
+            productName.style.display = 'block';
+            div.style.backgroundImage = "none";
+            div.style.backgroundColor = "pink";
+
+        });
+
+        a.addEventListener('mouseout', function handleMouseOut() {
+            productName.style.display = 'none';
+            div.style.backgroundImage = "linear-gradient(45deg, rgba(44, 174, 186, 0.1), rgba(44, 174, 186, 0.1)), url(../../img/products/thumbnails/" + item.id + ".png"+")"
+
+        });
+
         a.appendChild(div)
+        div.appendChild(productName)
         products.appendChild(a)
     })
 })
