f80af79234423662756ed4073ac766bdab44d8c7
Dániel Szabó <kingalistairlol@gmail.com>
2022-12-07 19:36:49 +0100

    Blog user view finished (for now)

--- js/admin/adminProductPopUpFetch.js
+++ js/admin/adminProductPopUpFetch.js
@@ -12,7 +12,7 @@ async function getIt() {
     const response = await fetch(apiUrl);
     const product = await response.json();
     let div = document.getElementById("image_column")
-    div.style.backgroundImage = div.style.backgroundImage = "linear-gradient(45deg, rgba(44, 174, 186, 0.1), rgba(44, 174, 186, 0.1)), url(../../img/products/thumbnails/" + id + ".png" + ")"
+    div.style.backgroundImage = div.style.backgroundImage = "linear-gradient(45deg, rgba(44, 174, 186, 0.1), rgba(44, 174, 186, 0.1)), url(../../../img/products/thumbnails/" + id + ".png" + ")"
     productName.textContent = product.name;
     productDescription.textContent = product.description;
     productPrice.textContent = product.price + ",-" ;
